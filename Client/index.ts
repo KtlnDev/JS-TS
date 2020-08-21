c3c5d456536d4f4e155e4a659132078939f56e37
WHQ_NT_DOMAIN\CB083492 <cata_banu98@yahoo.com>
2020-08-21 11:22:33 +0300

    fixed error

--- Client/index.ts
+++ Client/index.ts
@@ -3,20 +3,60 @@ import fetch from "node-fetch";
 const sumURL = "http://localhost:8080/sum/5/8";
 const subURL = "http://localhost:8080/sub/144/12";
 const mulURL = "http://localhost:8080/mul/7/6";
-const divURL = "http://localhost:8080/div/675/34";
+const divURL = "http://localhost:8080/div/144/12";
 
 fetch(sumURL)
   .then((body) => body.json())
-  .then((json) => console.log(json));
+  .then((json) => console.log("Sum: " + json));
 
 fetch(subURL)
   .then((body) => body.json())
-  .then((json) => console.log(json));
+  .then((json) => console.log("Subtraction: " + json));
 
 fetch(mulURL)
   .then((body) => body.json())
-  .then((json) => console.log(json));
+  .then((json) => console.log("Multiplication: " + json));
 
 fetch(divURL)
   .then((body) => body.json())
-  .then((json) => console.log(json));
+  .then((json) => console.log("Division: " + json));
+
+//exponential
+const paramsExp = new URLSearchParams();
+paramsExp.append("base", "2");
+paramsExp.append("exp", "3");
+(async () => {
+  const response = await fetch("http://localhost:8080/exponential", {
+    method: "POST",
+    body: paramsExp,
+  });
+  const json = await response.json();
+
+  console.log("Exponential: " + json);
+})();
+
+//log
+const paramsLog = new URLSearchParams();
+paramsLog.append("number", "10");
+(async () => {
+  const response = await fetch("http://localhost:8080/log", {
+    method: "POST",
+    body: paramsLog,
+  });
+  const json = await response.json();
+
+  console.log("Log: " + json);
+})();
+
+//sqrt
+const paramsSqrt = new URLSearchParams();
+paramsSqrt.append("number", "100");
+(async () => {
+  const response = await fetch("http://localhost:8080/sqrt", {
+    method: "POST",
+    body: paramsSqrt,
+  });
+  const json = await response.json();
+
+  console.log("Sqrt: " + json);
+})();
--- Server/index.ts
+++ Server/index.ts
@@ -10,28 +10,28 @@ app.get("/sum/:number1/:number2", (request, response) => {
   let number1 = parseInt(request.params.number1);
   let number2 = parseInt(request.params.number2);
   let result = number1 + number2;
-  response.send(number1 + " + " + number2 + " = " + result);
+  response.json(result);
 });
 
 app.get("/sub/:number1/:number2", (request, response) => {
   let number1 = parseInt(request.params.number1);
   let number2 = parseInt(request.params.number2);
   let result = number1 - number2;
-  response.send(number1 + " - " + number2 + " = " + result);
+  response.json(result);
 });
 
 app.get("/mul/:number1/:number2", (request, response) => {
   let number1 = parseInt(request.params.number1);
   let number2 = parseInt(request.params.number2);
   let result = number1 * number2;
-  response.send(number1 + " * " + number2 + " = " + result);
+  response.json(result);
 });
 
 app.get("/div/:number1/:number2", (request, response) => {
   let number1 = parseInt(request.params.number1);
   let number2 = parseInt(request.params.number2);
   let result = number1 / number2;
-  response.send(number1 + " / " + number2 + " = " + result);
+  response.json(result);
 });
 
 app.post("/exponential", (request, response) => {
@@ -40,19 +40,19 @@ app.post("/exponential", (request, response) => {
     exp: request.body.exp ? request.body.exp : "N/A",
   };
   let result = Math.pow(numbers.base, numbers.exp);
-  response.json(numbers.base + " ^ " + numbers.exp + " = " + result);
+  response.json(result);
 });
 
 app.post("/log", (request, response) => {
   let number = request.body.number ? request.body.number : "N/A";
   let result = Math.log(number);
-  response.json(`log(${number}) = ${result}`);
+  response.json(result);
 });
 
 app.post("/sqrt", (request, response) => {
   let number = request.body.number ? request.body.number : "N/A";
   let result = Math.sqrt(number);
-  response.json("sqrt(" + number + ") = " + result);
+  response.json(result);
 });
 
 app.listen(port, () => {
